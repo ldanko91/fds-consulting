cb84b17943d59b4f4287683a0d8e1b9a1df300e0
Damian <damian.e.ferrara@gmail.com>
2022-03-16 23:40:09 -0300

    incorporando php

---
+++ assets/vendor/php-email-form/php-email-form.php
@@ -0,0 +1,19 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "contacto@fdsconsulting.com.ar"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+/*     $last_name = $_POST['last_name']; */
+    $subject = $_POST['subject'];
+    
+    $message = $name . " " . " escribió lo siguiente:" . "\n\n" . $_POST['message'];
+  
+
+    $headers = "De:" . $from;
+  
+    mail($to,$subject,$message,$headers);
+
+    echo "Se envió tu mensaje" . $first_name . ", pronto nos pondremos en contacto con vos.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
