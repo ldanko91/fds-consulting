855227fbb8df9a6605187c1b855b01fe7ca8eb13
Damian <damian.e.ferrara@gmail.com>
2022-03-28 01:03:19 -0300

    php mail

--- assets/vendor/php-email-form/php-email-form.php
+++ assets/vendor/php-email-form/php-email-form.php
@@ -3,7 +3,7 @@ if(isset($_POST['submit'])){
     $to = "contacto@fdsconsulting.com.ar"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $name = $_POST['name'];
-/*     $last_name = $_POST['last_name']; */
+    /*$last_name = $_POST['last_name']; */
     $subject = $_POST['subject'];
     
     $message = $name . " " . " escribió lo siguiente:" . "\n\n" . $_POST['message'];
@@ -13,7 +13,7 @@ if(isset($_POST['submit'])){
   
     mail($to,$subject,$message,$headers);
 
-    echo "Se envió tu mensaje" . $first_name . ", pronto nos pondremos en contacto con vos.";
+    echo "Se envió tu mensaje" . $name . ", pronto nos pondremos en contacto con vos.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     } else {
         echo "No se pudo enviar tu mensaje";
